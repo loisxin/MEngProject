0b10c3f197498b4e068c033a08aa3e8360ef5553
Alexcyx <yc2494@cornell.edu>
2018-10-24 21:48:04 -0400

    First commit

---
+++ wizard1.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 50) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
